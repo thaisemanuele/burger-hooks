fc1f243b0c90f188d9f235981a09a9198770a064
thaisemanuele <thatitis@gmail.com>
2019-12-01 23:52:44 +0200

    Outputting multiple build controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,21 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(ingKey => {
             return [...Array(props.ingredients[ingKey])]    //Array of size ingredients.lenght
                 .map((_, index)=>{
                     return <BurgerIngredient key={ingKey + index} type={ingKey} />
                 });
         })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+    
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
