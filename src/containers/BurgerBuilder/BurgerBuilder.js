94049a7d9dbd269367b6eae801c38d166384ea99
thaisemanuele <thatitis@gmail.com>
2019-12-12 20:52:31 +0200

    Adding POST Request with axios

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,9 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
+import axios from '../../axios-orders';
+import { throwStatement } from '@babel/types';
+
 const INGREDIENT_PRICES = {
     salad: 0.3,
     cheese: 0.4,
@@ -79,7 +82,23 @@ class BurgerBuilder extends Component {
     }
     
     purchaseContinueHandler = () => {
-        alert('You continue!')
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name:'John Doe',
+                address: {
+                    street:'Street Fighter 2',
+                    zipCode:'G38PE',
+                    country: 'Estonia'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        };
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render() {
