bfa20689e25d529b28bfa6dffe96b7cc1c5e7369
thaisemanuele <thatitis@gmail.com>
2020-01-08 08:55:09 +0200

    Fetching orders via Redux

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -32,11 +32,51 @@ export const purchaseBurger = (orderData) => {
             .catch(error => {
                 dispatch(purchaseBurgerFailed(error));
             }); 
-    }
+    };
 };
 
 export const purchaseInit = () => {
     return {
         type: actionTypes.PURCHASE_INIT
-    }
+    };
+};
+
+export const fetchOrdersSuccess = (orders) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_SUCCESS,
+        orders: orders
+    };
+};
+
+export const fetchOrdersFailed = (error) => {
+    return {
+        type: actionTypes.FETCH_INGREDIENTS_FAILED,
+        error: error
+    };
+};
+
+export const fetchOrdersStart = () => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_START
+    };
+};
+
+export const fetchOrders = () => {
+    return dispatch => {
+        dispatch(fetchOrdersStart());
+        axios.get('orders.json')
+                .then(res => {
+                    const fetchedOrders = [];
+                    for (let key in res.data) {
+                        fetchedOrders.push({
+                            ...res.data[key],
+                            id: key
+                        });
+                    }
+                    dispatch(fetchOrdersSuccess(fetchedOrders));
+                })
+                .catch(error => {
+                    dispatch(fetchOrdersFailed(error));
+                });    
+    };
 };
